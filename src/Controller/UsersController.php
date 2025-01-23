886106a613aa9588e21b1952e2020ae7beb018be
Obellay <oceanebellay85340@gmail.com>
2025-01-23 10:13:22 +0100

    changement user

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -52,5 +52,25 @@ final class UsersController extends AbstractController
         return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
     }
 
+    #[Route('/profile', name: 'app_profile', methods: ['GET'])]
+    public function profile(EntityManagerInterface $entityManager) : Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            
+            return $this->redirectToRoute('app_login');
+        }
+    
+
+        $tweets = $entityManager
+        ->getRepository(Tweets::class)
+        ->findBy(['user' => $user], ['createdAt' => ['DESC']]);
+
+        return $this->render('users/profile.html.twig',[
+            'user' =>$user,
+            'tweets' => $tweets,
+        ]);
+    }
+
   
 }
