67bd9636634ef39da8f945a56a061cf6b55c0a55
Taieb <catherine.taieb@hotmail.com>
2025-01-23 10:12:32 +0100

    Modification du tweet form

--- src/Form/TweetsType.php
+++ src/Form/TweetsType.php
@@ -7,6 +7,7 @@ use App\Entity\Tweets;
 use App\Entity\Users;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,19 +16,18 @@ class TweetsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('createdAt', null, [
-                'widget' => 'single_text'
-            ])
-            ->add('updateAt', null, [
-                'widget' => 'single_text'
-            ])
+            // ->add('createdAt', null, [
+            //     'widget' => 'single_text'
+            // ])
+            // ->add('updateAt', null, [
+            //     'widget' => 'single_text'
+            // ])
             ->add('user', EntityType::class, [
                 'class' => Users::class,
 'choice_label' => 'id',
             ])
-            ->add('content', EntityType::class, [
-                'class' => Contents::class,
-'choice_label' => 'id',
+            ->add('content', TextareaType::class, [
+                'required' => true,
             ])
         ;
     }
