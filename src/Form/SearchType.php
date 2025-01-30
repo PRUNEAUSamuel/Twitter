4ea2070deed46592c1f3c85dbd91c57c6b771446
Obellay <oceanebellay85340@gmail.com>
2025-01-30 09:57:28 +0100

    recherche et affichage profile terminer

---
+++ src/Form/SearchType.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('query', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Rechercher un utilisateur...', 'class' => 'form-control']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // You can define the data class if needed
+        ]);
+    }
+}
